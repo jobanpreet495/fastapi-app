b21878777dc0c3757a62d0dad126b689506d3f30
Jobanpreet S <pythondev.erginous@gmaiil.com>
2024-12-10 13:29:30 +0530

    ygdis

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ async def add_numbers(num1: float = Form(...), num2: float = Form(...)):
 
 # Run the FastAPI app with uvicorn
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000 , reload=True)
